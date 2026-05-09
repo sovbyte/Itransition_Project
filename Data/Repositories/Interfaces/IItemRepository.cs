79434114c29ba811920b56c7c70ec5e39d7c870a
sovbyte <sovbyte@gmail.com>
2026-05-09 05:28:46 +0500

    ItemRepository.cs

--- Data/Repositories/Interfaces/IItemRepository.cs
+++ Data/Repositories/Interfaces/IItemRepository.cs
@@ -4,5 +4,9 @@ namespace Itransition_Project.Data.Repositories.Interfaces;
 
 public interface IItemRepository : IRepository<Item>
 {
-    //TODO
+    Task<IEnumerable<Item>> GetByInventoryIdAsync(int inventoryId, int skip, int take);
+    
+    Task<Item?> GetWithDetailsAsync(int id);
+    
+    Task<bool> ExistsByCustomIdAsync(int inventoryId,string customId);
 }
--- Data/Repositories/ItemRepository.cs
+++ Data/Repositories/ItemRepository.cs
@@ -1,9 +1,34 @@
 using Itransition_Project.Data.Repositories.Interfaces;
 using Itransition_Project.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Itransition_Project.Data.Repositories;
 
 public class ItemRepository(ApplicationDbContext context) : BaseRepository<Item>(context), IItemRepository
 {
-    //TODO
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<IEnumerable<Item>> GetByInventoryIdAsync(int inventoryId, int skip, int take)
+    {
+        return await _context.Items.
+            Where(i => i.InventoryId == inventoryId)
+            .OrderByDescending(i => i.CreatedAt)
+            .Skip(skip)
+            .Take(take)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<Item?> GetWithDetailsAsync(int id)
+    {
+        return await _context.Items
+            .Include(i => i.Inventory)
+            .FirstOrDefaultAsync(i => i.Id == id);
+    }
+
+    public async Task<bool> ExistsByCustomIdAsync(int inventoryId, string customId)
+    {
+        return await _context.Items
+            .AnyAsync(i => i.InventoryId == inventoryId && i.CustomIdValue == customId);
+    }
 }
